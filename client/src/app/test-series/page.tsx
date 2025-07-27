bd73f53c9457d6864a941a4a5f3562117dad05c3
Priyanshu <priyanshupatel405@gmail.com>
2025-07-27 11:08:45 +0530

    Feat: test series footer

--- client/src/app/test-series/page.tsx
+++ client/src/app/test-series/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import TestCard from '../free-tests/components/TestCard'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const page = () => {
   return (
     <>
     <Navbar/>
     <TestCard/>
-    
+    <Footer/>
     </>
   )
 }
