814766098b96da98adf32cb74c597876a12a6ec3
Priyanshu <priyanshupatel405@gmail.com>
2025-07-27 11:07:33 +0530

    Feat:Test series

---
+++ client/src/app/test-series/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TestCard from '../free-tests/components/TestCard'
+import Navbar from '../components/Navbar'
+
+const page = () => {
+  return (
+    <>
+    <Navbar/>
+    <TestCard/>
+    
+    </>
+  )
+}
+export default page
